89e179f2803976f16a94d5e0a31d28db4874923d
1234567891234567891234567898123456789 <thisgameisg@gmail.com>
2025-02-17 15:13:14 +0800

    Added player Jump
    Player can now jump and is affected by gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,19 +11,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera _ThirdPersonCamera;
     private int _currentCam = 1;
 
+    // Animator and input
     [SerializeField] private Animator _animator;
     [SerializeField] private CharacterController _characterController;
     [SerializeField] private PlayerInput _playerInput;
     private InputActionAsset _inputActions;
-
+    // Mouse delta for camea 
     [SerializeField] private float mouseSensitivity;
     private InputAction lookAction;
     private Vector2 mouseDelta;
     private float cameraPitch = 0f;
+
+    // Gravity
+    [SerializeField] private float Gravity;
+    private Vector3 GravityVelocity = Vector3.zero;
+
+    // Jump
+    private Vector3 JumpVelocity;
+    private InputAction JumpAction;
+    private float JumpHeight = 3.0f;
+
+
+
     void Awake()
     {
         _characterController = GetComponent<CharacterController>();
-        lookAction = _playerInput.actions["Look"]; // Reference the "Look" action
     }
     // Start is called before the first frame update
     void Start()
@@ -31,11 +43,16 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         _inputActions = _playerInput.actions;
+        lookAction = _playerInput.actions["Look"];
+        JumpAction = _playerInput.actions["Jump"];
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Fall();
+        Jump();
         Vector2 input =
         _inputActions["Move"].ReadValue<Vector2>();
         Vector3 moveDirection = new Vector3(input.x, 0, input.y);
@@ -56,6 +73,8 @@ public class PlayerController : MonoBehaviour
             _animator.SetBool("IsRunning", true);
         else
             _animator.SetBool("IsRunning", false);
+
+        _characterController.Move((JumpVelocity) * Time.deltaTime);
 
     }
 
@@ -96,7 +115,6 @@ public class PlayerController : MonoBehaviour
             cameraPitch -= mouseY;
             cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90);
             _FirstPersonCamera.transform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
-            Debug.Log(cameraPitch);
         }
     }
     public void Look()
@@ -107,6 +125,25 @@ public class PlayerController : MonoBehaviour
             float mouseX = mouseDelta.x * mouseSensitivity * Time.deltaTime;
             // Rotate the camera vertically horizontally
            gameObject.transform.Rotate(Vector3.up * mouseX);
+        }
+    }
+
+    public void Fall()
+    {
+        //falling
+        GravityVelocity.y += Gravity * Time.deltaTime;
+        if (_characterController.isGrounded && JumpVelocity.y < 0)
+        {
+            JumpVelocity.y = -2f;
+        }
+        JumpVelocity.y += Gravity * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (JumpAction.IsPressed() && _characterController.isGrounded)
+        {
+            JumpVelocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity);
         }
     }
 }

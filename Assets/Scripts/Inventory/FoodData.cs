2db137b1857aa74690021b7fd7b6b0550a2ef3b0
Kentyty <tangykenneth2@gmail.com>
2025-02-19 14:45:43 +0800

    food base
    Added in base scriptable object for food and drink

---
+++ Assets/Scripts/Inventory/FoodData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FoodData", menuName = "Food Data", order =2)]
+public class FoodData : ItemData
+{
+    public float HungerRestore;
+    public float ThirstRestore;
+}
